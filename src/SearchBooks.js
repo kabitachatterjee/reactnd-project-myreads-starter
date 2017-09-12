70fcb91dc12b500071c9aa9286226847be00862a
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-09-12 15:18:46 -0700

    added a search input form on the search books screen

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class SearchBooks extends Component {
   render() {
     return (
-      <div>Search Books!</div>
+      <div className='search-books-bar'>
+      <Link to='/' className='close-search'>Close</Link>
+      <form className='earch-books-input-wrapper'>
+      <input  type='text' placeholder='Search by name or author'/>
+      </form>
+      </div>
     )
   }
 }
