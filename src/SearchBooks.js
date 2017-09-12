b25a202511a8d783e32f702487d6449eecb02e5a
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-09-12 15:42:31 -0700

    added serialize for the input for search books

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import serializeForm from 'form-serialize';
 
 class SearchBooks extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const values = serializeForm(e.target, {hash: true})
+    console.log(values);
+    if(this.props.onSearchBooks) {
+      this.props.onSearchBooks(values)
+    }
+  }
   render() {
     return (
       <div className='search-books-bar'>
       <Link to='/' className='close-search'>Close</Link>
-      <form className='earch-books-input-wrapper'>
-      <input  type='text' placeholder='Search by name or author'/>
+      <form onSubmit={this.handleSubmit} className='search-books-input-wrapper'>
+      <input type='text' name='name' placeholder='Search by name or author'/>
       </form>
       </div>
     )
