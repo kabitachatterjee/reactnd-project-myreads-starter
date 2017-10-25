4c22882402355108d416e300c5a759ae36402b73
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-10-25 10:22:20 -0700

    history.history.push for adding books from searchBooks

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import * as BooksAPI from './BooksAPI';
 import './App.css';
 import BookList from './BookList';
 import SearchBooks from './SearchBooks';
-import { Route } from 'react-router-dom';
+import { Route,Link } from 'react-router-dom';
 
 class BooksApp extends React.Component {
   state = {
@@ -37,12 +37,14 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
+      <Route exact path='/' render={() => (<BookList books={this.state.allBooks} shelfUpdate={this.shelfUpdate} />)} />
       <Route path='/search' render={(history) => (<SearchBooks books={this.state.books}
         onShelfSelect={(event) => {
             this.shelfUpdate(event)
-          this.context.router.transitionTo('/')
-        }}/>)} />
-        <Route exact path='/' render={() => (<BookList books={this.state.allBooks} shelfUpdate={this.shelfUpdate} />)} />
+            history.history.push('/')
+        }}/>
+      )} />
+
         </div>
     )
   }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import serializeForm from 'form-serialize';
 import Shelf from './Shelf';
 import * as BooksAPI from './BooksAPI';
 import BookList from './BookList';
